d08b3f49c37e12634f62e32292146ddeb3544f8c
vsheviakhova <65824737+vsheviakhova@users.noreply.github.com>
2022-06-01 22:45:03 +0300

    Update 1.3_.py

--- 1.3_.py
+++ 1.3_.py
@@ -1,4 +1,4 @@
-cube_side = int(input())
+cube_side = int(input("Введите сторону куба: "))
 square_cube = (cube_side ** 2) * 6
 volume_cube = cube_side ** 3
 print(f"Площадь поверхностей куба: {square_cube}")
